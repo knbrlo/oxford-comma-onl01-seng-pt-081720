b70fdff1d975f66d7f7ad92afccb2fd32f638731
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 21:32:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,12 @@
 def oxford_comma(array)
+  final_string
   if array.size == 1
-    array.to_s
+    final_string = array.to_s
   elsif array.size == 2
-    array.join(", ")
+    final_string = array.join(", ")
   elsif array.size >= 3
     array[-1] = "and #{array[-1]}"
-    array.join(", ")
+    final_string = array.join(", ")
   end
- array
+ final_string
 end
